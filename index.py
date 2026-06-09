6a983f2fa1d2815cb0f50029e4b7c081a9b86adc
Yasmine Khemir <yasminekhemir471@gmail.com>
2026-06-10 00:06:46 +0200

    create dictionary

--- index.py
+++ index.py
@@ -1,3 +1,6 @@
+dictionnaire_mots = {"vie": "existance"}
 print("Entrez votre phrase : ")
 phrase = input()
-print( phrase )
+if mot in range dictionnaire_mots[]:
+  print(dictionnaire_mots[mot])
+print(dictionnaire_mots["vie"])
